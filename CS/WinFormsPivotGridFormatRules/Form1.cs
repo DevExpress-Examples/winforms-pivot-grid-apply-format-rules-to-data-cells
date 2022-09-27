66c6b815066c2cbc1904351d9813cb97d7ee4be5
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-09-27 14:16:25 +0400

    Update Form1.cs

--- CS/WinFormsPivotGridFormatRules/Form1.cs
+++ CS/WinFormsPivotGridFormatRules/Form1.cs
@@ -2,32 +2,24 @@
 using DevExpress.XtraPivotGrid;
 using DevExpress.XtraEditors;
 
-namespace WinFormsPivotGridFormatRules
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace WinFormsPivotGridFormatRules {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
             salesPersonTableAdapter1.Fill(nwindDataSet1.SalesPerson);
-
             // Creates a new FormatRule object.
             PivotGridFormatRule newRule = new PivotGridFormatRule();
-
             // Sets a Measure.
             newRule.Measure = fieldExtendedPrice;
-
             // Creates and specifies a new Settings object.
             newRule.Settings = new FormatRuleFieldIntersectionSettings{
                 Column = fieldOrderYear1, 
                 Row = fieldSalesPerson 
             };
-
             // Creates a new Rule object and sets its parameters.
             newRule.Rule = new FormatConditionRuleDataBar{ 
                 PredefinedName = "Yellow Gradient" 
             };
-
             // Adds the rule to the collection.
             pivotGridControl1.FormatRules.Add(newRule);
         }
